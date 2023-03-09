8314d3f62810c36d877a89df66efbd713af177de
VetrovMilka <VetrovMilka@github.com>
2023-03-09 18:19:51 +0200

    Start

---
+++ src/test/java/com/vetrovmilka/craigslist/CraigslistApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vetrovmilka.craigslist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CraigslistApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
